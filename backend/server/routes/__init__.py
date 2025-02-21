06e2508020149d5b0f839ca1cdf7b789f9cab687
JCamyre <jwcamry03@gmail.com>
2025-02-20 21:36:15 -0800

    Add suggestions router.

--- backend/server/routes/__init__.py
+++ backend/server/routes/__init__.py
@@ -1,4 +1,5 @@
 from .expenses import router as expenses_router
 from .goals import router as goals_router
+from .suggestions import router as suggestions_router
 
-__all__ = ["expenses_router", "goals_router"]
+__all__ = ["expenses_router", "goals_router", "suggestions_router"]
