13c53f02b561fd29e6996927803b4196f208f9cf
Michelle Kim <mkheeseo@gmail.com>
2025-02-20 23:24:33 -0800

    Test folder setup

---
+++ frontend/budgetbuddy/src/__tests__/page.test.tsx
@@ -0,0 +1,9 @@
+
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/page'
+ 
+test('Page', () => {
+  render(<Page />)
+  expect(1+1).toBe(2)
+})
--- frontend/budgetbuddy/src/app/page.tsx
+++ frontend/budgetbuddy/src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>Welcom to BudgetBuddy.</main>
+    <main>Welcome to BudgetBuddy.</main>
   );
 }
