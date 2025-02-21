a6450db7aa693498ad05af4e6a4d265ae6dcd932
JCamyre <jwcamry03@gmail.com>
2025-02-20 23:04:58 -0800

    Remove boilerplate endpoints.

--- backend/server/main.py
+++ backend/server/main.py
@@ -13,24 +13,11 @@ app.include_router(expenses_router)
 app.include_router(goals_router)
 app.include_router(suggestions_router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/health")
+async def health():
+    return {"message": "Health Check"}
 
-@app.get("/test_db")
-async def test_db():
-    supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
-    data = supabase.table("expenses").select("*").execute()
-    return data
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/items/")
-async def create_item(item: dict):
-    return item
-
+# TODO: Update user's budget based on receipt
 @app.post("/photo-receipts/")
 async def create_photo_receipt(file: UploadFile = File(...)):
     # Save the uploaded file temporarily
