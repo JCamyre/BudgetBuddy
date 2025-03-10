b4f63c2e8adc6604c3bf26e1bf5ec694319b2b28
Miche Kim <mkheeseo@gmail.com>
2025-03-09 19:38:22 -0700

    Toggle components on personalized insights page

--- frontend/budgetbuddy/src/app/insights/page.tsx
+++ frontend/budgetbuddy/src/app/insights/page.tsx
@@ -1,25 +1,52 @@
+"use client"
 import React from 'react'
 
 const Insights = () => {
+  const [collapse1, setCollapse1] = React.useState(true)
+  const [collapse2, setCollapse2] = React.useState(true)
+  const [collapse3, setCollapse3] = React.useState(true)
+  
   return (
     <div className = "min-h-screen">
       <div className="p-8 bg-white text-green-900">
         <h1 className="text-4xl font-bold mb-8">Personalized Insights</h1>
       </div>
 
-      <div className = "p-8 m-8 cursor-pointer bg-green-900 hover:bg-green-700 text-white flex-auto min-w-screen max-w-screen">
-        Budgeting Tips
-        <div className="collapse bg-base-100 border-base-300 border">
-          <input type="checkbox" />
-          <div className="collapse-title font-semibold">How do I create an account?</div>
-          <div className="collapse-content text-sm">
-            Click the "Sign Up" button in the top right corner and follow the registration process.
-          </div>
+      <div>
+        <div className = "p-8 m-8 cursor-pointer bg-green-900 hover:bg-green-700 text-white flex-auto min-w-screen max-w-screen"
+            onClick={() => setCollapse1(!collapse1)}>
+          Your Budget: Tips and Tricks
         </div>
+        {
+          (collapse1) ?
+            <div></div> :
+            <div className= "ml-8">Budgeting Tips</div>
+        }
       </div>
-      <div className = "p-8 m-8 cursor-pointer bg-green-900 hover:bg-green-700 text-white flex-auto min-w-screen max-w-screen">test block 1</div>
-      <div className = "p-8 m-8 cursor-pointer bg-green-900 hover:bg-green-700 text-white flex-auto min-w-screen max-w-screen">test block 1</div>
-      
+
+      <div>
+        <div className = "p-8 m-8 cursor-pointer bg-green-900 hover:bg-green-700 text-white flex-auto min-w-screen max-w-screen"
+            onClick={() => setCollapse2(!collapse2)}>
+          How does your spending compare to other users?
+        </div>
+        {
+          (collapse2) ?
+            <div></div> :
+            <div className= "ml-8">Budgeting Tips</div>
+        }
+      </div>
+
+      <div>
+        <div className = "p-8 m-8 cursor-pointer bg-green-900 hover:bg-green-700 text-white flex-auto min-w-screen max-w-screen"
+            onClick={() => setCollapse3(!collapse3)}>
+          Additional
+        </div>
+        {
+          (collapse3) ?
+            <div></div> :
+            <div className= "ml-8">Budgeting Tips</div>
+        }
+      </div>
     </div>
   )
 }
